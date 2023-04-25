4b7069b1c45c5aac2a2d5f30b86c56b0131bf83a
DunioE <126677297+DunioE@users.noreply.github.com>
2023-04-25 19:45:08 +0100

    Assets De Menu + Menu de GameOver

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public static bool isGameOver;
+    public GameObject gameOverScreen;
+
+    private void Awake()
+    {
+        isGameOver = false;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isGameOver)
+        {
+            gameOverScreen.SetActive(true);
+        }
+        
+    }
+
+    public void ReplayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
