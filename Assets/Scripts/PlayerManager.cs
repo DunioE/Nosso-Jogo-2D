e750e81997892da5b6b5fedbd11c89cb82f44d54
DunioE <126677297+DunioE@users.noreply.github.com>
2023-04-25 20:13:22 +0100

    CheckPoints

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,9 +7,12 @@ public class PlayerManager : MonoBehaviour
     public static bool isGameOver;
     public GameObject gameOverScreen;
 
+    public static Vector2 lastCheckPointPos = new Vector2(-8.7f,-2.9f);
+
     private void Awake()
     {
         isGameOver = false;
+        GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPos;
     }
 
     void Start()
