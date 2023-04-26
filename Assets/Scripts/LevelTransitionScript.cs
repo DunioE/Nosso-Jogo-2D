a4f9ba370ef9524233f69e820927a75919db2b89
DunioE <126677297+DunioE@users.noreply.github.com>
2023-04-26 15:03:44 +0100

    Fix Erro entre niveis
    Erro ao transitar do nivel 01 para o 02, através do Menu, tinha erro de o personagem desaparecer mas foi resolvido

--- Assets/Scripts/LevelTransitionScript.cs
+++ Assets/Scripts/LevelTransitionScript.cs
@@ -3,11 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelTransitionScript : MonoBehaviour
 {
+    public int levelIndex;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level02");
+            MenuEvents menuEvents = FindObjectOfType<MenuEvents>();
+            if (menuEvents != null)
+            {
+                menuEvents.LoadLevel(levelIndex);
+            }
+            else
+            {
+                SceneManager.LoadScene(levelIndex);
+            }
         }
     }
 }
